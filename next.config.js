099acab862c1909f70fa89e2b41927556de83de4
Lolwethu Bongoza <lolwethu.bongoza@wintechnologies.net>
2020-01-22 16:34:51 +0200

    next conf

--- next.config.js
+++ next.config.js
@@ -1,32 +1,31 @@
-const withOffline = require('next-offline');
+// Progressive Web App: Add service worker with network-first strategy.
+// Network-first strategy means that if there is no internet connection,
+// the browser will use files previously saved locally to the user’s device instead.
+// AKA Offline Mode!
+const withOffline = require("next-offline");
 
 const nextConfig = {
+    target: "serverless",
     workboxOpts: {
-        swDest: process.env.NEXT_EXPORT ? 'service-worker.js' : 'static/service-worker.js',
-        globPatterns: ['static/**/*'],
-        globDirectory: '.',
-
+        swDest: "static/service-worker.js",
         runtimeCaching: [
-            serviceWorkerBase(/.*\.(?:css|scss|less)/, 'StaleWhileRevalidate', 'css-cache', 20, 12 * 60 * 60),
-            serviceWorkerBase(/.*\.(?:png|jpg|jpeg|svg|gif).*/, 'StaleWhileRevalidate', 'image-cache', 500, 24 * 60 * 60),
-            serviceWorkerBase(/.*\.(?:mp4|webm).*/, 'StaleWhileRevalidate', 'media-cache', 30, 24 * 60 * 60),
-            serviceWorkerBase(/.*\.(?:avatars1.githubusercontent.com).*/, 'StaleWhileRevalidate', 'html-cache', 50, 24 * 60 * 60),
-            serviceWorkerBase(/.*\.(?:js|jsx|ts)/, 'StaleWhileRevalidate', 'script-cache', 20, 24 * 60 * 60),
-            serviceWorkerBase(/.*\.(?:woff|woff2|ttf|eot|otf)/, 'StaleWhileRevalidate', 'font-cache', 20, 24 * 60 * 60),
+            {
+                urlPattern: /^https?.*/,
+                handler: "networkFirst",
+                options: {
+                    cacheName: "https-calls",
+                    networkTimeoutSeconds: 15,
+                    expiration: {
+                        maxEntries: 150,
+                        maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
+                    },
+                    cacheableResponse: {
+                        statuses: [0, 200]
+                    }
+                }
+            }
         ]
     }
-}
-
-function serviceWorkerBase(pattern, cacheType, cacheName, maxEntries, maxAgeSeconds = 24 * 60 * 60) {
-    return {
-        urlPattern: pattern,
-        handler: cacheType,
-        options: {
-            cacheName,
-            expiration: { maxEntries, maxAgeSeconds },
-            cacheableResponse: { statuses: [0, 200] }
-        }
-    };
-}
+};
 
 module.exports = withOffline(nextConfig);
